7a6d51a6d0c14364437adb8755c1467956e0cf91
pol-yana <yana.polchyn@gmail.com>
2020-04-17 23:33:38 +0300

    add ShowIncompleteTask component

--- src/components/ShowIncompleteTask.js
+++ src/components/ShowIncompleteTask.js
@@ -1,9 +1,22 @@
-//import ReactDOM from 'react-dom'
+//import ReactDOM from "react-dom";
+import React from "react";
 import { Component } from "react";
+import IncomleteTask from "./IncompleteTask";
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 //import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export class ShowIncomleteTask extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+    console.log("showIncompleteTask", this.state);
+  }
+
+  render() {
+    return <IncomleteTask list={this.state.list} />;
+  }
 }
 
 export default ShowIncomleteTask;
